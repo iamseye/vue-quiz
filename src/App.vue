8de359c8a475ab2b2a05382499b63bc6538c5307
Jess <iamseye@gmail.com>
2019-08-24 16:55:22 -0400

    ADD: component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,89 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Simpe quiz</h1>
+    <Quiz v-if="quizIndex <= quizes.length-1" :quiz="quizes[quizIndex]" :quizIndex="quizIndex" @changeQuiz="changeQuiz"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quiz from './components/Quiz.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      quizIndex: 0,
+      quizes: [
+        {
+          "question": "What is Finn's favourite color?",
+          "answers": [
+                {
+                    "text": "Red",
+                    "id": 1,
+                    "correct": true
+                },
+                {
+                    "text": "Green",
+                    "id": 2,
+                    "correct": false
+                },
+                {
+                    "text": "Blue",
+                    "id": 3,
+                    "correct": false
+                }
+            ]
+        },
+        {
+          "question": "What is Jake's favourite Food?",
+          "answers": [
+                {
+                    "text": "ApplePie",
+                    "id": 1,
+                    "correct": false
+                },
+                {
+                    "text": "Curry",
+                    "id": 2,
+                    "correct": true
+                },
+                {
+                    "text": "Peabut butter",
+                    "id": 3,
+                    "correct": false
+                }
+            ]
+        },
+        {
+          "question": "What is BMO's favourite place?",
+          "answers": [
+              {
+                  "text": "Home",
+                  "id": 1,
+                  "correct": false
+              },
+              {
+                  "text": "Kingdom",
+                  "id": 2,
+                  "correct": false
+              },
+              {
+                  "text": "Hill",
+                  "id": 3,
+                  "correct": true
+              }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+    changeQuiz(quizIndex) {
+      this.quizIndex = quizIndex;
+    }
+  },
   components: {
-    HelloWorld
+    Quiz
   }
 }
 </script>
---
+++ src/components/Quiz.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    
+    {{ quizIndex }}
+
+    <div v-if="quizIndex > 0"> 
+      {{ quiz.question }}
+      <ol>
+        <li v-for="(answer, index) in quiz.answers" :key='index'>
+          <label class="radio">
+            <input type="radio" name="answer">
+            {{ answer.text }}
+          </label>
+        </li>
+      </ol>
+      <button @click="changeQuiz(quizIndex)">Next</button>
+    </div>
+
+    <div v-else>
+      Welcome to the simple quiz
+      <button @click="changeQuiz(quizIndex)">Start</button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quiz',
+  props: ['quiz', 'quizIndex'],
+  methods: {
+    changeQuiz(quizIndex) {
+        this.$emit('changeQuiz', quizIndex +=1 );
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
